738afb9ffa863bd0c85f822ef7a8a7c777b01a62
양정훈 <luciferkala@gmail.com>
2019-12-31 13:20:55 +0900

    Feat : make youtube route and add dependancy
    youtube route 생성 및 axios, short-unique-id, moment module --save 완료

---
+++ routes/api/youtube/youtube.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+const defaultRes = require('../../../module/utils/utils');
+const statusCode = require('../../../module/utils/statusCode');
+const resMessage = require('../../../module/utils/responseMessage')
+const db = require('../../../module/pool');
+const jwtUtils = require('../../../module/jwt');
+
+
+router.get('/', async (req, res) => {
+    // Instantiate
+    res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.LOGIN_SUCCESS))
+})
+
+router.get('/info', async (req, res) => {
+    // Instantiate
+    res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.LOGIN_SUCCESS))
+})
+
+
+router.get('/:idx', async (req, res) => {
+    // Instantiate
+    res.status(200).send(defaultRes.successTrue(statusCode.OK, resMessage.LOGIN_SUCCESS))
+})
+
+
+
+
+module.exports = router;
