11df532f8915c38cb2adedcd2696b4b3eb303cc3
YunJegal <ddr04014@gmail.com>
2020-01-02 14:18:11 +0900

    Fix : add changeKRName, changeENGName in common.js

--- module/common.js
+++ module/common.js
@@ -5,6 +5,38 @@ const authUtil = {
     curTime : () => {
         return moment().format('YY.MM.DD HH:mm:ss')
     },
+
+    changeKRName = (categoryCode)  => {
+        if (categoryCode === '0101') {
+            return '프리미엄'
+        } else if (categoryCode === '0102') {
+            return '뷰티'
+        } else if (categoryCode === '0103') {
+            return '맛집'
+        } else if (categoryCode === '0104') {
+            return '여행'
+        } else if (categoryCode === '0105') {
+            return '기타'
+        } else if (categoryCode === '0201') {
+            return '법률'
+        }
+    },
+
+    changeENGName = (categoryCode)  => {
+        if (categoryCode === '0101') {
+            return 'Primium'
+        } else if (categoryCode === '0102') {
+            return 'Beauty'
+        } else if (categoryCode === '0103') {
+            return 'Restaurant'
+        } else if (categoryCode === '0104') {
+            return 'Travel'
+        } else if (categoryCode === '0105') {
+            return 'Other'
+        } else if (categoryCode === '0201') {
+            return 'Law'
+        }
+    }
 };
 
 module.exports = authUtil;
