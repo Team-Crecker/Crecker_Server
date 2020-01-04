39ca88e768b4ddff433e6ee45454827996b69962
YunJegal <ddr04014@gmail.com>
2020-01-04 10:39:57 +0900

    Fix : modify write

--- routes/api/advertise/ad.js
+++ routes/api/advertise/ad.js
@@ -284,21 +284,26 @@ router.post('/write', authUtils.isLoggedin, async (req, res) => {
     // console.log(req.body);
 
     const getWriteQuery = 'INSERT INTO Plan (title, subtitle,youtubeUrl, phone, location, planTitle, planContents, refUrl,isAgree, userIdx) VALUES(?,?,?,?,?,?,?,?,?,?)'
-    const postWriteQuery = 'INSERT INTO UserAd (userIdx, adIdx, progress, createAt) VALUES (?,?,?,?)';
-    const getPlanQuery = `SELECT b.planIdx FROM UserAd as a JOIN Plan as b ON a.planIdx=b.planIdx WHERE a.userIdx=${req.decoded.idx} AND a.adIdx=${req.body.adIdx}`
+    const getPlanIdxQuery = `SELECT planIdx FROM Plan WHERE userIdx=${req.decoded.idx} order by planIdx DESC`
+
+    const getPlanIdxResult = await db.queryParam_None(getPlanIdxQuery);
+    const planIdx = getPlanIdxResult[0].planIdx
+
+    const postWriteQuery = 'INSERT INTO UserAd (userIdx, adIdx, planIdx, progress, createAt) VALUES (?,?, ?, ?,?)';
+    // const getPlanQuery = `SELECT b.planIdx FROM UserAd as a JOIN Plan as b ON a.planIdx=b.planIdx WHERE a.userIdx=${req.decoded.idx} AND a.adIdx=${req.body.adIdx}`
     console.log("getPlanQuery", getPlanQuery);
     // const getPlanQuery = `SELECT planIdx FROM Plan WHERE phone='${req.body.phone}'`;
-    const writeTransaction = await db.Transaction(async connection => {
-        const getWriteResult = await connection.query(getWriteQuery, [req.body.title, req.body.subtitle, req.body.youtubeUrl, req.body.phone, req.body.location, req.body.planTitle, req.body.planContents, req.body.refUrl, req.body.isAgree, req.decoded.idx]);
-        const postWriteResult = await connection.query(postWriteQuery, [req.decoded.idx, req.body.adIdx, 1, moment().format('YYYY-MM-DD HH:mm:ss')]);
-    })
+    // const writeTransaction = await db.Transaction(async connection => {
+    const getWriteResult = await db.queryParam_Arr(getWriteQuery, [req.body.title, req.body.subtitle, req.body.youtubeUrl, req.body.phone, req.body.location, req.body.planTitle, req.body.planContents, req.body.refUrl, req.body.isAgree, req.decoded.idx]);
+    const postWriteResult = await db.queryParam_Arr(postWriteQuery, [req.decoded.idx, req.body.adIdx, planIdx, 1, moment().format('YYYY-MM-DD HH:mm:ss')]);
+    // })
     const getPlanResult = await db.queryParam_None(getPlanQuery);
     console.log(getPlanResult);
-    const putUserAdQuery = `UPDATE UserAd SET planIdx = ${getPlanResult[0].planIdx} WHERE adIdx=${req.body.adIdx} AND userIdx=${req.decoded.idx}`;
+    // const putUserAdQuery = `UPDATE UserAd SET planIdx = ${getPlanResult[0].planIdx} WHERE adIdx=${req.body.adIdx} AND userIdx=${req.decoded.idx}`;
 
-    const putUserAdResult = await db.queryParam_None(putUserAdQuery);
+    // const putUserAdResult = await db.queryParam_None(putUserAdQuery);
 
-    if (!writeTransaction || !putUserAdResult) {
+    if (!getWriteResult || !postWriteResult) {
         res.status(200).send(defaultRes.successFalse(statusCode.DB_ERROR, resMessage.DB_ERROR));
     } else {
         res.status(200).send(defaultRes.successTrue(statusCode.OK, "기획서 작성 성공"));
